7fa9012e8350fc50ed2bde3c408b48e1d044067d
Andrea Nassif <andrea.e.nassif@gmail.com>
2022-07-09 16:00:13 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos al curso de ReactJS!
         </p>
+        <button>Comenzar!</button>
         <a
           className="App-link"
           href="https://reactjs.org"
