1dedb5cab0e92736d5dd115530bb4652d42378d0
Andrea Nassif <andrea.e.nassif@gmail.com>
2022-07-30 14:34:03 -0300

    add img y css

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,10 +4,10 @@ const Item = ({productos})=> {
     return (
         <div class="card">
             <div class="card-image">
-                <img src="{productos.img}" alt="images"/>
-                <span class="card-title">{productos.nombre}</span>
+                <img class="img-card" src={productos.img} alt="images"/>
             </div>
             <div class="card-content">
+                <span class="card-title">{productos.nombre}</span>
                 <p>{productos.tipo}</p>
                 <p>{productos.desc}</p>
                 <span><p>${productos.precio}</p></span>
